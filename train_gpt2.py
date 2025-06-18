724aa2f90e5929b08cec21cb130bde217056c5b9
Aryan Jain <145840890+hotrod462@users.noreply.github.com>
2025-06-19 03:58:51 +0530

    grad accum

--- train_gpt2.py
+++ train_gpt2.py
@@ -265,7 +265,19 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
 
-train_loader = DataLoaderLite(B=2, T= 1024)
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+total_batch_size = 524288 # 2**19, ~0.5M, in number of tokens
+B = 1 #micro batch size
+T = 1024 #Sequence length
+assert total_batch_size % ( B * T) == 0, "make sure total_batch_size is divisible by B * T"
+grad_accum_steps = total_batch_size // (B* T)
+print(f"total desired btach size: {total_batch_size}")
+print(f"=> calculated grad accumulation steps: {grad_accum_steps}")
+
+train_loader = DataLoaderLite(B=B, T= T)
 
 torch.set_float32_matmul_precision('high')
 
@@ -298,12 +310,17 @@ optimizer = model.configure_optimizers(weight_decay = 0.1, learning_rate = 6e-4,
 
 for step in range(max_steps):
     t0 = time.time()
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits, loss = model(x, y)
-    loss.backward()
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+        loss = loss / grad_accum_steps
+        loss_accum += loss.detach() 
+        loss.backward()
+
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     lr = get_lr(step)
     for param_group in optimizer.param_groups:
@@ -312,8 +329,9 @@ for step in range(max_steps):
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0) * 1000 #time diff in milliseconds
+    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
     tokens_per_sec = (train_loader.B * train_loader.T)/(t1 - t0)
-    print(f"step {step} | loss: {loss.item()} | norm: {norm: .4f} | dt: {dt:.2f}ms, tok/sec: {tokens_per_sec}")
+    print(f"step {step} | loss: {loss_accum.item()} | norm: {norm: .4f} | dt: {dt:.2f}ms, tok/sec: {tokens_per_sec}")
     
 import sys; sys.exit(0)
 
